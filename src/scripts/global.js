05d05b90453e801a509e644c07b66d79cf245e10
dave98 <jose.mamani.vilca@ucsp.edu.pe>
2022-10-21 15:38:03 -0500

    Feat: Header responsivity

--- src/scripts/global.js
+++ src/scripts/global.js
@@ -1,9 +1,26 @@
 
 export function globalEvents(){
     console.log("Started global interactions...");
+
     let searchForm = document.querySelector(".search-form");
+    let navbar = document.querySelector(".navbar");
     
+    // Search icon interaction
     document.getElementById("search-btn").addEventListener("click", () => {
         searchForm.classList.toggle("active");
+        navbar.classList.remove("active");
     })
+
+    // Menu icon iteraction (Only when media query)
+    document.getElementById("menu-btn").addEventListener("click", () => {
+        navbar.classList.toggle("active");
+        searchForm.classList.remove("active");
+    })
+    
+
+    // Deleting most added interactions when scrolling
+    window.onscroll = () => {
+        searchForm.classList.remove("active");
+        navbar.classList.remove("active");
+    }
 }
