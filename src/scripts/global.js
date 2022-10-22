4ed7238bb797a0ee1b19d6416e5d26189ad0ae9f
dave98 <jose.mamani.vilca@ucsp.edu.pe>
2022-10-21 23:08:22 -0500

    Feat: Products display

--- src/scripts/global.js
+++ src/scripts/global.js
@@ -1,26 +1,78 @@
+let searchForm = document.querySelector(".search-form");
+let navbar = document.querySelector(".navbar");
+let filterBtn = document.querySelectorAll(".filter-buttons .buttons");
+// Temporary, replace with backend behavior
+let filterItem = document.querySelectorAll(".products .box-container .product-box")
 
-export function globalEvents(){
-    console.log("Started global interactions...");
-
-    let searchForm = document.querySelector(".search-form");
-    let navbar = document.querySelector(".navbar");
-    
-    // Search icon interaction
+// Search icon interaction
+function searchFormInteraction(){
     document.getElementById("search-btn").addEventListener("click", () => {
         searchForm.classList.toggle("active");
         navbar.classList.remove("active");
     })
+}
 
-    // Menu icon iteraction (Only when media query)
+// Menu icon iteraction (Only when media query)
+function menuIconInteraction(){
     document.getElementById("menu-btn").addEventListener("click", () => {
         navbar.classList.toggle("active");
         searchForm.classList.remove("active");
     })
-    
+}
 
-    // Deleting most added interactions when scrolling
+function addSwipperController(){
+    let swiper = new Swiper(".home-slider", {
+        spaceBetween: 0,
+        centeredSlides: true,
+        loop: true,
+        autoplay: {
+            delay: 5000,
+            disableOnInteraction: false,
+        },
+        navigation: {
+            nextEl: ".swiper-button-next",
+            prevEl: ".swiper-button-prev",
+        }
+    });
+}
+
+function filterInteraction(){
+    filterBtn.forEach(category => {
+        category.addEventListener("click", () => {
+            filterBtn.forEach((btn) => { btn.classList.remove("active"); }) // Removing active state for all category
+            category.classList.add("active");   // Adding active state for current selected category
+
+            let dataFilter = category.getAttribute("data-filter");  // Obtaining category filter value
+            filterItem.forEach(product => {
+                // Default values for all products
+                product.classList.remove("active");
+                product.classList.add("hide");
+                // Filtering
+                if(product.getAttribute("data-item") == dataFilter || 
+                    dataFilter == "all"){
+                        product.classList.remove("hide");
+                        product.classList.add("active");
+                }
+            })
+        })
+    })
+}
+
+// Default behavior when scrolling
+function behaviorOnScroll(){
     window.onscroll = () => {
         searchForm.classList.remove("active");
         navbar.classList.remove("active");
     }
+
+}
+
+
+export function globalEvents(){
+    console.log("Started global interactions...");
+    searchFormInteraction();    
+    menuIconInteraction();
+    addSwipperController();
+    filterInteraction();
+    behaviorOnScroll();
 }
